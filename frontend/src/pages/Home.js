1c7ed2f9e7f76ecda05ad5982e0f7f2637406c50
Laupaka <jeremy.beaugendre@gmail.com>
2026-03-02 10:55:12 +0100

    Fix: retrait import ArtisanCard inutilisé dans Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getArtisansDuMois } from '../services/api';
-import ArtisanCard from '../components/ArtisanCard';
 import './Home.scss';
 
 const Home = () => {
